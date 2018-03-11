ac296e9dc5013704f58418488348855281f95372
Coding Bear <surfwon@naver.com>
2018-03-11 16:04:12 +0000

    데이터베이스 시딩

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        if (config('database.default') != 'sqlite') {
+			DB::statement('SET FOREIGN_KEY_CHECKS=0');
+		}
+		// Model::unguard(); // 라라벨 5.1에서 사용
+		App\User::truncate(); // 테이블에 담긴 모든 데이터를 지운다.
+		$this->call(UsersTableSeeder::class);
+		
+		App\Article::truncate();
+		$this->call(ArticlesTableSeeder::class);
+		
+		// Model::reguard();
+		if (config('database.default') !== 'sqlite') {
+			DB::statement('SET FOREIGN_KEY_CHECKS=1');
+		}
     }
 }
