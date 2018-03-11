c6d1a534d376b03713cd513ec383a03cadf25d1c
Coding Bear <surfwon@naver.com>
2018-03-11 04:56:42 +0000

    라우팅

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,33 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+//code: 기본 라우팅
+// Route::get('/', function () {
+// 	return view('welcome');
+// });
 
-Route::get('/', function () {
-    return view('welcome');
+// code: 라우팅에 대한 문자열 반환
+// Route::get('/', function () {
+//     return '<h1>Hello Foo</h1>';
+// });
+
+// code: URL 파라미터 패턴
+// Route::pattern('foo', '[0-9a-zA-Z]{3}'); // 정규표현식으로 URL 파라미터 패턴을 강제
+// Route::get('/{foo?}', function($foo ='bar') {
+//     return $foo;
+// });
+
+// Route::get('/{foo?}', function ($foo = 'bar') {
+// 	return $foo;
+// })->where('foo', '[0-9a-zA-Z]{4}'); // where() 메서드 체인
+
+// code: 라우트 이름
+Route::get('/', [
+	'as' => 'home',
+	function () {
+		return '제이름은 "home" 입니다.';
+	}
+]);
+Route::get('/home', function () {
+	return redirect(route('home'));
 });
